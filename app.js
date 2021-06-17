d4bead58b90ab7606ae725602362e3907d85687f
Tatyana Loza <jes220@ukr.net>
2021-06-18 02:35:48 +0300

    +++

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const cors = require('cors')
 const helmet = require('helmet')
 const rateLimit = require('express-rate-limit')
 const boolParser = require('express-query-boolean')
+const path = require('path')
 const { limiterAPI } = require('./helpers/constants')
-
 const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
+require('dotenv').config()
+const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(helmet())
+app.use(express.static(path.join(__dirname, AVATAR_OF_USERS)))
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json({ limit: 10000 }))
@@ -22,8 +26,9 @@ app.use(boolParser())
 
 app.use('/api/', rateLimit(limiterAPI))
 
-app.use('/api/users', require('./routes/api/users'))
+app.use('/api/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
+
 
 app.use((req, res) => {
   res.status(404).json({ status: 'error', code: 404, message: 'Not found' })
@@ -36,6 +41,10 @@ app.use((err, req, res, next) => {
     code: status,
     message: err.message,
   })
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason)
 })
 
 module.exports = app
--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,11 @@ const Users = require('../repositories/users')
 const { HttpCode } = require('../helpers/constants')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const fs = require('fs/promises')
+
 const SECRET_KEY = process.env.SECRET_KEY
+
+
 
 const register = async (req, res, next) => {
   try {
@@ -62,8 +66,49 @@ const logout = async (req, res, next) => {
   }
 }
 
+
+
+  //  Local Upload
+const path = require('path')
+  const UploadAvatarService = require('../services/local-upload')
+ 
 const avatars = async (req, res, next) => {
-  res.json({ message: 'Done' })
+  try {
+    const id = req.user.id
+    const uploads = new UploadAvatarService(process.env.AVATAR_OF_USERS)
+    const avatarUrl = await uploads.saveAvatar({ idUser: id, file: req.file })
+
+    try {
+      await fs.unlink(path.join(process.env.AVATAR_OF_USERS, req.user.avatar))
+    } catch (e) {
+      console.log(e.message)
+    }
+
+    await Users.updateAvatar(id, avatarUrl)
+    res.json({ status: 'success', code: 200, data: { avatarUrl } })
+  } catch (error) {
+    next(error)
+  }
 }
+
+
+// const UploadAvatarService = require('../services/cloud-upload')
+// const avatars = async (req, res, next) => {
+//   try {
+//     const id = req.user.id
+//     const uploads = new UploadAvatarService()
+//     const { idCloudAvatar, avatarUrl } = await uploads.saveAvatar(
+//       req.file.path,
+//       req.user.idCloudAvatar,
+//     )
+
+//     //  delete file on folder uploads
+//     await fs.unlink(req.file.path)
+//     await Users.updateAvatar(id, avatarUrl, idCloudAvatar)
+//     res.json({ status: 'success', code: 200, data: { avatarUrl } })
+//   } catch (error) {
+//     next(error)
+//   }
+// }
 
 module.exports = { register, login, logout, avatars }
